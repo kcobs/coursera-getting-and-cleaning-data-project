7782272f88173bfef688133b1e287fe6fa668309
Kimberly Coburn <kimberly.coburn@kcobs.org>
2016-04-03 17:32:58 -0500

    Added files via upload

---
+++ run_analysis.R
@@ -0,0 +1,78 @@
+#run.analysis.R
+
+library(data.table)
+library(dplyr)
+
+
+#get data into 1 data table
+
+featureNames <- read.table("features.txt")
+activityLabels <- read.table("activity_labels.txt", header = FALSE)
+
+#read training data
+subjectTrain <- read.table("train/subject_train.txt", header = FALSE)
+activityTrain <- read.table("train/y_train.txt", header = FALSE)
+featuresTrain <- read.table("train/X_train.txt", header = FALSE)
+
+#read test data
+subjectTest <- read.table("test/subject_test.txt", header = FALSE)
+activityTest <- read.table("test/y_test.txt", header = FALSE)
+featuresTest <- read.table("test/X_test.txt", header = FALSE)
+
+#merge training data and test data
+subject <- rbind(subjectTrain, subjectTest)
+activity <- rbind(activityTrain, activityTest)
+features <- rbind(featuresTrain, featuresTest)
+
+#Name colummns
+colnames(features) <- t(featureNames[2])
+
+#merge and store in completedata
+colnames(activity) <- "Activity"
+colnames(subject) <- "Subject"
+completeData <- cbind(features,activity,subject)
+
+#extract only measurements on the mean and standard deviation for each measurement
+columnsWithMeanSTD <- grep(".*Mean.*|.*Std.*", names(completeData), ignore.case=TRUE)
+
+#add activity columns and subject columns to examine dimensions of completeData
+requiredColumns <- c(columnsWithMeanSTD, 562, 563)
+dim(completeData)
+
+extractedData <- completeData[,requiredColumns]
+dim(extractedData)
+
+#name the activities
+extractedData$Activity <- as.character(extractedData$Activity)
+for (i in 1:6){
+    extractedData$Activity[extractedData$Activity == i] <- as.character(activityLabels[i,2])
+}
+
+#factor the activity variable
+extractedData$Activity <- as.factor(extractedData$Activity)
+
+names(extractedData)
+
+names(extractedData)<-gsub("Acc", "Accelerometer", names(extractedData))
+names(extractedData)<-gsub("Gyro", "Gyroscope", names(extractedData))
+names(extractedData)<-gsub("BodyBody", "Body", names(extractedData))
+names(extractedData)<-gsub("Mag", "Magnitude", names(extractedData))
+names(extractedData)<-gsub("^t", "Time", names(extractedData))
+names(extractedData)<-gsub("^f", "Frequency", names(extractedData))
+names(extractedData)<-gsub("tBody", "TimeBody", names(extractedData))
+names(extractedData)<-gsub("-mean()", "Mean", names(extractedData), ignore.case = TRUE)
+names(extractedData)<-gsub("-std()", "STD", names(extractedData), ignore.case = TRUE)
+names(extractedData)<-gsub("-freq()", "Frequency", names(extractedData), ignore.case = TRUE)
+names(extractedData)<-gsub("angle", "Angle", names(extractedData))
+names(extractedData)<-gsub("gravity", "Gravity", names(extractedData))
+
+names(extractedData)
+
+#create a second tdy data set with the averages of each variable for each acitivity and subject
+extractedData$Subject <- as.factor(extractedData$Subject)
+extractedData <- data.table(extractedData)
+
+#create tidyData and write to Tidy.txt file
+tidyData <- aggregate(. ~Subject + Activity, extractedData, mean)
+tidyData <- tidyData[order(tidyData$Subject,tidyData$Activity),]
+write.table(tidyData, file = "Tidy.txt", row.names = FALSE)
